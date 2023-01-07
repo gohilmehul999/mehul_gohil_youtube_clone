05d066731ccd2f9586be841de0b9e0760ce10f3a
gohilmehul999 <gohilmehul1582002@gmail.com>
2023-01-07 12:31:25 +0530

    new changes

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,10 +1,15 @@
+import { HomeComponent } from './../side-list-components/home/home/home.component';
 import { MainComponent } from './main/main.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{
   path:'',
-  component:MainComponent
+  component:MainComponent,
+  children:[{
+    path:'',
+    component:HomeComponent
+  }]
 }];
 
 @NgModule({
